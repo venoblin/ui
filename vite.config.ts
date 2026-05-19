487c948ae2f4a94388e86746a4d4f7d5e25c49eb
Jonathan <villagomezhjonathan@gmail.com>
2026-05-18 22:49:09 -0400

    finished setting up library

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ['lib'] })],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       formats: ['es']
-    }
+    },
+    copyPublicDir: false
   }
 })
