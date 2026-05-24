76c0aa47ae78f40c4c9ee996c8cb562c1cdfdcfb
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2026-05-24 13:07:55 -0400

    created master stylesheet

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -1,6 +1,7 @@
 import type { ButtonProps } from './Button'
 import type { Size, Variant } from '../../types/shared'
 import { classConcat } from '../../utils'
+import '../../styles/master.css'
 import '../../styles/globals.css'
 import './Button.css'
 
