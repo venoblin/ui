fa73fd11c053de474379b862a72d603a9c9d6c6b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2026-06-28 19:14:24 -0400

    added typea

--- lib/components/TextInput/TextInput.d.ts
+++ lib/components/TextInput/TextInput.d.ts
@@ -1,4 +1,8 @@
+export type TextInputType = 'text' | 'email' | 'password'
+
 export interface TextInputProps extends React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
-> {}
+> {
+  type: TextInputType
+}
